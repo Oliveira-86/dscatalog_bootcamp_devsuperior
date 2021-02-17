ef2dbbb9e75467ef16a5d204432fd722c5d46703
Egberto Oliveira <egberto.duarte.14@gmail.com>
2021-02-16 22:05:11 -0300

    Entities user and role, ORM mapping, database seeding

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,16 +1,74 @@
-import React from 'react';
+import { makeResquest } from 'core/utils/request';
+import React, { useState } from 'react';
 import BaseForm from '../../BaseForm';
 
+type FormState = {
+    name: string;
+    price: string;
+    category: string;
+}
 
 const Form = () => {
-    return(
-        <BaseForm title="CADASTRAR UM PRODUTO">
-            <div className="row">
-                <div className="col-6">
-                    <input type="text" className="form-control"/>
+
+    const [formData, setFormData] = useState<FormState>({
+        name: '',
+        price: '',
+        category: ''
+    });
+
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setFormData(data => ({...data, [name]: value}))
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const payload = {
+            ...formData,
+            imgUrl: 'https://images7.kabum.com.br/produtos/fotos/115737/console-sony-playstation-5-midia-fisica_1598984720_g.jpg',
+            categories: [{ id: formData.category }]
+        }
+
+        makeResquest({ url:'/products', method: 'POST', data: payload })
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <BaseForm title="CADASTRAR UM PRODUTO">
+                <div className="row"> 
+                    <div className="col-6">
+                        <input
+                            value={formData.name}
+                            name="name"
+                            type="text"
+                            className="form-control mb-5"
+                            onChange={handleOnChange}
+                            placeholder="Nome do Produto"
+                        />
+                        <select
+                            value={formData.category}
+                            className="form-control mb-5"
+                            onChange={handleOnChange}
+                            name="category"
+                        >
+                            <option value="1">Livros</option>
+                            <option value="3">Computadores</option>
+                            <option value="2">Eletrônicos</option>
+                        </select>
+                        <input
+                            value={formData.price}
+                            name="price"
+                            type="text"
+                            className="form-control"
+                            onChange={handleOnChange}
+                            placeholder="Preço"
+                        />
+                    </div>
                 </div>
-            </div>
-        </BaseForm>
+            </BaseForm>
+        </form>
     )
 }
 
