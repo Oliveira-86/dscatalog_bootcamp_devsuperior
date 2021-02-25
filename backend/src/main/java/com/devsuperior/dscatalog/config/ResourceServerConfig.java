8bafb52cc37e49ae6cc809dd2a4f85ab35a5b6bd
Egberto Oliveira <egberto.duarte.14@gmail.com>
2021-02-25 17:07:22 -0300

    H2 Security configuration

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,5 +1,8 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
+import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -14,9 +17,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Autowired
+	private org.springframework.core.env.Environment env;
+	
+	@Autowired
 	private JwtTokenStore tokenStore;
 	
-	private static final String[] PUBLIC = { "/oauth/token" };
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
 	
 	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
 	
@@ -29,6 +35,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+		//H2
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
